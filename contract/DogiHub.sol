b5fe907adcf133a9c5968891b32fd84c2ff86df8
belew <belew@belewtech.com>
2018-10-22 00:01:19 +0800

    update

--- contract/DogiHub.sol
+++ contract/DogiHub.sol
@@ -9,45 +9,46 @@ contract DogiHub is Ownable{
   using SafeMath for uint256;
 
 
-  event TransferToBypass(address indexed bypassContract, address indexed from, address indexed to, uint256 id, uint256 value);
-  event TransferFromBypass(address indexed bypassContract, address indexed from, address indexed to, uint256 id, uint256 value);
+  event TransferOut(address indexed bypassContract, address indexed from, address indexed to, uint256 id, uint256 value);
+  event TransferIn(address indexed bypassContract, address indexed from, address indexed to, uint256 id, uint256 value);
 
   mapping (address => address) bypassContract;
-  mapping (address => uint256) transferToBypassId;
-  mapping (address => uint256) transferFromBypassId;
+  mapping (address => uint256) transferOutId;
+  mapping (address => uint256) transferInId;
 
   function addContract(address _contractAddress, address _bypassAddress) public onlyOwner returns (bool){
   	require(_bypassAddress != address(0));
 
   	bypassContract[_contractAddress] = _bypassAddress;
-  	//transferToBypassId[_contractAddress] = 0;
-  	//transferFromBypassId[_contractAddress] = 0;
+  	//transferOutId[_contractAddress] = 0;
+  	//transferInId[_contractAddress] = 0;
 
   	return true;
   }
 
-  function transferToBypass(address _contractAddress, address _to, uint256 _value) public returns (bool) {
+  function transferOut(address _contractAddress, address _to, uint256 _value) public returns (bool) {
     require(_to != address(0));
 
     IERC20 _contract = IERC20(_contractAddress);
 
     _contract.transferFrom(msg.sender, this, _value);
-    emit TransferToBypass(bypassContract[_contractAddress], msg.sender, _to, transferToBypassId[_contractAddress], _value);
+    emit TransferOut(bypassContract[_contractAddress], msg.sender, _to, transferOutId[_contractAddress], _value);
 
-    transferToBypassId[_contractAddress].add(1);
+    transferOutId[_contractAddress].add(1);
     return true;
   }
 
-  function transferFromBypass(uint256 _id, address _contractAddress, address _from, address _to, uint256 _value) public onlyOwner returns (bool){
+//change to array
+  function transferIn(uint256 _id, address _contractAddress, address _from, address _to, uint256 _value) public onlyOwner returns (bool){
     require(_to != address(0));
-    require(_id == transferFromBypassId[_contractAddress]);
+    require(_id == transferInId[_contractAddress]);
 
     IERC20 _contract = IERC20(_contractAddress);
 
     _contract.transfer(_to, _value);
-    emit TransferFromBypass(bypassContract[_contractAddress], _from, _to, _id, _value);
+    emit TransferIn(bypassContract[_contractAddress], _from, _to, _id, _value);
 
-    transferFromBypassId[_contractAddress] = _id.add(1);
+    transferInId[_contractAddress] = _id.add(1);
 
     return true;
   }
--- js/main.js
+++ js/main.js
@@ -1,22 +1,37 @@
-const erc20Abi = [];
+const Web3 = require('web3');
+const fs = require('fs');
+const net = require('net');
+let config = JSON.parse(fs.readFileSync(__dirname + '/../json/config.json').toString());
+if (config.WebsocketProvider !== undefined) {
+    var web3 = new Web3(new Web3.providers.WebsocketProvider(config.WebsocketProvider));
+}
+if (config.mainnet.HttpProvider !== undefined) {
+    var mWeb3 = new Web3(new Web3.providers.HttpProvider(config.mainnet.HttpProvider));
+}
+let privateKey = fs.readFileSync(__dirname + '/../privatekey/private.key').toString();
+let account = web3.eth.accounts.wallet.add(privateKey);
+let mainnetPrivateKey = fs.readFileSync(__dirname + '/../privatekey/mainnet_private.key').toString();
+let mainnetAccount = mWeb3.eth.accounts.wallet.add(mainnetPrivateKey);
+let hubAddress;
 
-const mainHubAddress = '';
-const mainnetWeb3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-const bypassHubAddress = '';
-const bypassWeb3 = web3;
+let mHubContract = new mWeb3.eth.Contract(config.mainnet.hubAbi, config.mainnet.hubAddress);
+let bHubContract = new web3.eth.Contract(config.hubAbi, hubAddress);
 
-const erc20 = web3.eth.contract(erc20Abi);
-var erc20Instance = erc20.at(erc20Address);
-// watch for an event with {some: 'args'}
-var transferToBypass = erc20Instance.TransferToBypass(null, {fromBlock: 0, toBlock: 'latest'});
-transferToBypass.watch(function(error, result){
- if (!error){
+let mPreList = [];
+let bPreList = [];
 
- }
-});
-// would get all past logs again.
-var myResults = transferToBypass.get(function(error, logs){ ... });
-...
-// would stop and uninstall the filter
-transferToBypass.stopWatching();
+//TODO get fromBlock1
+let fromBlock1 = 0;
+mHubContract.events.transferOut({fromBlock:fromBlock1})
+.on('data', function(event){
+	let data = event.returnValues;
+
+	//bPreList add data;
+})
+.on('changed', function(event){
+    // remove event from local database
+})
+.on('error', console.error);
+
+//send bPreList and clear bPreList
